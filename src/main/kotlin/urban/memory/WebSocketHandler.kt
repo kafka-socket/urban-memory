7656faca889d78824409ebd169053550190deda4
Alexey Nikitin <nikitin@bookmate.com>
2019-01-02 06:09:32 +0300

    Implements ws ping/pong

--- src/main/kotlin/urban/memory/WebSocketHandler.kt
+++ src/main/kotlin/urban/memory/WebSocketHandler.kt
@@ -1,17 +1,27 @@
 package urban.memory
 
+import kotlinx.coroutines.*
 import org.eclipse.jetty.websocket.api.Session
 import org.eclipse.jetty.websocket.api.WebSocketListener
+import org.eclipse.jetty.websocket.api.WebSocketPingPongListener
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
 
-class WebSocketHandler : WebSocketListener {
+class WebSocketHandler : WebSocketListener, WebSocketPingPongListener {
     private var session: Session? = null
+    private var heartbeatJob: Job? = null
     private val logger: Logger = LoggerFactory.getLogger(WebSocketHandler::class.java)
+
+    companion object {
+        private const val HEARTBEAT_INTERVAL = 5_000L
+    }
 
     override fun onWebSocketConnect(s: Session?) {
         session = s
         logger.info("Incoming connection with [${userAgent()}]")
+        heartbeatJob = heartbeat()
     }
 
     override fun onWebSocketBinary(payload: ByteArray?, offset: Int, len: Int) {
@@ -22,8 +32,18 @@ class WebSocketHandler : WebSocketListener {
         logger.info("Message received [$message] from session [${userAgent()}]")
     }
 
+    override fun onWebSocketPing(payload: ByteBuffer?) {
+        logger.info("Ping received: [$payload]")
+        session?.remote?.sendPong(payload)
+    }
+
+    override fun onWebSocketPong(payload: ByteBuffer?) {
+        logger.info("Pong received: [${StandardCharsets.UTF_8.decode(payload)}]")
+    }
+
     override fun onWebSocketClose(statusCode: Int, reason: String?) {
         logger.info("Connection closed with code $statusCode:[$reason]")
+        heartbeatJob?.cancel()
     }
 
     override fun onWebSocketError(cause: Throwable?) {
@@ -32,5 +52,14 @@ class WebSocketHandler : WebSocketListener {
 
     private fun userAgent() : String {
         return session?.upgradeRequest?.getHeader("user-agent") ?: "Unknown"
+    }
+
+    private fun heartbeat() : Job {
+        return GlobalScope.launch {
+            while (isActive) {
+                session?.remote?.sendPing(ByteBuffer.wrap("beat".toByteArray()))
+                delay(HEARTBEAT_INTERVAL)
+            }
+        }
     }
 }
